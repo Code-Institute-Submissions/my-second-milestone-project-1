5ea92005c93e55a3844aa189cab41f807a80ca59
oakerele-web <omoakerele@gmail.com>
2020-09-02 00:01:40 +0000

    added guessesRemaining, guessesMade, gameState and gameWon

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
     // Game Variables
     var mysteryNumber = 50;
     var playersGuess =0;
-    
+    var guessesRemaining = 10;
+    var guessesMade = 0;
+    var gameState = "";
+    var gameWon = false;
+
     // The input and output field
     var input = document.querySelector("#input");
     var output = document.querySelector("#output");
@@ -15,17 +19,43 @@
     }
 
     function playGame(){
+        guessesRemaining --;
+        guessesMade ++;
+        gameState = " Guess: " + guessesMade + ", Remaining: " + guessesRemaining;
+
         playersGuess = parseInt(input.value);
 
         if(playersGuess > mysteryNumber){
-            output.innerHTML = "That's too high.";
+            output.innerHTML = "That's too high." + gameState;
+
+        // Check for the end of the game
+        if (guessesRemaining < 1){
+            endGame();
+        }
         }
         else if(playersGuess < mysteryNumber){
-            output.innerHTML = "That's is too low.";
+            output.innerHTML = "That's too low." + gameState;
+
+        // Check for the end of the game
+        if (guessesRemaining < 1){
+            endGame();
+        }
         }
         else if(playersGuess === mysteryNumber){
-            output.innerHTML = "You got it!";
+            gameWon = true;
+            endGame();
         }
     }
 
-    
+    // Add endGame function
+
+    function endGame(){
+        if (gameWon){
+            output.innerHTML = "Yes, it's " + mysteryNumber + "!" + "<br>"
+            + "It only took you " + guessesMade + " guesses.";
+        }
+        else {
+            output.innerHTML = "No more guesses remaining!" + "<br>"
+            + "The number was: " + mysteryNumber + ".";
+        }
+    }
