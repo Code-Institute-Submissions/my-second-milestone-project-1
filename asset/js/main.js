4992d8607063f70da9e8e0e7164b1ae4b996c28a
oakerele-web <omoakerele@gmail.com>
2020-09-27 20:41:58 +0000

    Added New Game button

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,84 +1,71 @@
-    // Game Variables
-    var mysteryNumber = Math.floor(Math.random() * 100);
-    
-    var playersGuess = 0;
-    var guessesRemaining = 10;
-    var guessesMade = 0;
-    var gameState = "";
-    var gameWon = false;
+// Game variables
+var mysteryNumber = Math.floor(Math.random() * 100);
 
-    // The input and output fields
-    var input = document.querySelector("#input");
-    var output = document.querySelector("#output");
+playersGuess = 0;
+var guessesRemaining = 10;
+var guessesMade = 0;
+var gameState = "";
+var gameWon = false;
 
-    // The Button
-    var button = document.querySelector("#button");    
-    button.addEventListener("click", clickHandler, false);
+// The Input and GuessResult fields
+var input = document.querySelector(".input");
+var guessResult = document.querySelector(".guessResult");
 
-    // Listen for Enter key presses
-    window.addEventListener("keydown", keydownHandler, false);
+// The button
+var guessButton = document.querySelector(".guessButton");
+guessButton.addEventListener("click", clickHandler, false);
 
-    function keydownHandler(event){
-        if(event.keyCode === 13){
-            validateInput();
-        }
+ // Restart Button
+function newGame() {
+window.location.reload();
     }
 
-    // Ensuring only numbers are entered
-    function clickHandler(){
-        validateInput();
+window.addEventListener("keydown", keydownHandler, false);
+function keydownHandler(event) {
+    if(event.keyCode === 13) {
+        playGame();
     }
+}
 
-    function validateInput(){
-        playersGuess = parseInt(input.value);
-        if(isNaN(playersGuess)){
-            output.innerHTML = "Please enter a number";
-        }else{
-            playGame();
-        }
-    }
+function clickHandler() {
+    playGame();
+}
 
-    function playGame(){
-        guessesRemaining --;
-        guessesMade ++;
-        gameState = " Guess: " + guessesMade + ", Remaining: " + guessesRemaining;
+function playGame() {
+    guessesRemaining--;
+    guessesMade++;
+    gameState = " Guess: " + guessesMade + ", Remaining: " + guessesRemaining;
+ 
+   playersGuess = parseInt(input.value);
 
-        if(playersGuess > mysteryNumber){
-            output.innerHTML = "That's higher." + gameState;
+    if(playersGuess > mysteryNumber) {
+        guessResult.innerHTML = "That's higher." + gameState;
 
         // Check for the end of the game
-        if (guessesRemaining < 1){
+        if (guessesRemaining < 1) {
             endGame();
         }
-        }
-        else if(playersGuess < mysteryNumber){
-            output.innerHTML = "That's lower." + gameState;
+    } else if (playersGuess < mysteryNumber) {
+        guessResult.innerHTML = "That's lower." + gameState;
 
         // Check for the end of the game
-        if (guessesRemaining < 1){
+        if (guessesRemaining < 1) {
             endGame();
         }
-        }
-        else if(playersGuess === mysteryNumber){
-            gameWon = true;
-            endGame();
-        }
-     
+    } else if (playersGuess === mysteryNumber) {
+        gameWon = true;
+        endGame();
     }
-
-    // Add endGame function
-
-    function endGame(){
-        if (gameWon){
-            output.innerHTML = "Yes, it's " + mysteryNumber + "!" + "<br>"
-            + "It took you " + guessesMade + " guess(es).";
+}
+    function endGame() {
+        if (gameWon) {
+            guessResult.innerHTML = "Yes, it's " + mysteryNumber + "!" + "<br>" + "It took you " + guessesMade + " guess(es)"
+        } else {
+            guessResult.innerHTML = "No more guesses remaining!" + "<br>" + "The number was: " + mysteryNumber + ".";
         }
-        else {
-            output.innerHTML = "No more guesses remaining!" + "<br>"
-            + "The number was: " + mysteryNumber + ".";
-        }
-        // Add disable the button to ensure game cannot play anymore after endgame
-        button.removeEventListener("click", clickHandler, false);
+
+        // Disable the button
+        guessButton.removeEventListener("click", clickHandler, false);
         button.disabled = true;
 
         // Disable the enter key
@@ -86,5 +73,4 @@
 
         // Disable the input field
         input.disabled = true;
-    }
-
+    }  
